ee606fa84c5a4e67be71a7cb7433d2b906d53d40
RaspyPiano24270 <gerner.eric13@gmail.com>
2024-11-20 21:10:42 -0600

    PlayerController update
    updated code to use Unity's input binding feature for more quick and efficient execution. also readd the arrow keys so users can use either WASD or the arrow keys, whichever is their preference.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,16 @@ using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
-       // Start is called before the first frame update
+    // Start is called before the first frame update
+    public InputAction MoveAction;
     void Start(){
-
+        MoveAction.Enable();
     }
-       // Update is called once per frame
+    // Update is called once per frame
     void Update(){
-        float horizontal = 0.0f;
-        if (Keyboard.current.aKey.isPressed){
-            horizontal = -1.0f;
-        } else if (Keyboard.current.dKey.isPressed){
-            horizontal = 1.0f;
-        }
-        Debug.Log(horizontal);
-
-        float verticle = 0.0f;
-        if (Keyboard.current.wKey.isPressed){
-            verticle = 1.0f;
-        } else if (Keyboard.current.sKey.isPressed){
-            verticle = -1.0f;
-        }
-        Debug.Log(verticle);
-        
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * verticle;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 0.1f;
         transform.position = position;
     }
 }
