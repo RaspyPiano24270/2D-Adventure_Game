45140c2df3c05c8c1ed95c072c3f2e144fc6dc0d
RaspyPiano24270 <gerner.eric13@gmail.com>
2024-11-20 20:20:35 -0600

    updeated PlayerController
    added vertical movement and changed keys to WASD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,14 +11,24 @@ public class PlayerController : MonoBehaviour{
        // Update is called once per frame
     void Update(){
         float horizontal = 0.0f;
-        if (Keyboard.current.leftArrowKey.isPressed){
+        if (Keyboard.current.aKey.isPressed){
             horizontal = -1.0f;
-        } else if (Keyboard.current.rightArrowKey.isPressed){
+        } else if (Keyboard.current.dKey.isPressed){
             horizontal = 1.0f;
         }
         Debug.Log(horizontal);
+
+        float verticle = 0.0f;
+        if (Keyboard.current.wKey.isPressed){
+            verticle = 1.0f;
+        } else if (Keyboard.current.sKey.isPressed){
+            verticle = -1.0f;
+        }
+        Debug.Log(verticle);
+        
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * verticle;
         transform.position = position;
     }
 }
