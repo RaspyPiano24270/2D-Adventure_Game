342f0c880c33e8ee40fc58b480e28e2fd62091ed
RaspyPiano24270 <146394354+RaspyPiano24270@users.noreply.github.com>
2024-12-02 14:19:40 -0600

    health
    started adding a health system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,15 @@ public class PlayerController : MonoBehaviour{
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+    //variables related to character health
+    public int maxHealth = 5;
+    int currentHealth;
     // Start is called before the first frame update
     void Start(){
         MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
     // Update is called once per frame
     void Update(){
@@ -20,7 +25,11 @@ public class PlayerController : MonoBehaviour{
         
     }
     void FixedUpdate(){
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+    void ChangeHealth(int amount){
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
